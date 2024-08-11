7fa1fd0301b5f25d1099b2bcc5c31db579761a11
FA design <fadesign001@gmail.com>
2024-08-11 23:21:52 +0530

    add top button

--- src/js/script.js
+++ src/js/script.js
@@ -42,3 +42,32 @@ var swiper = new Swiper(".mySwiper", {
         }
     }
   });
+
+
+  const currentYear = new Date().getFullYear();
+    
+  // Set the year to the HTML tag with id 'year'
+  document.getElementById('copyright-year').textContent = currentYear;
+
+
+  //Scroll to Top
+  const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function() {
+      scrollFunction();
+  };
+
+  function scrollFunction() {
+      if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+          scrollToTopBtn.style.display = "block";
+      } else {
+          scrollToTopBtn.style.display = "none";
+      }
+  }
+
+  // When the user clicks on the button, scroll to the top of the document
+  scrollToTopBtn.addEventListener("click", function() {
+      document.body.scrollTop = 0; // For Safari
+      document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  });
